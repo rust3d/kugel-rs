907958b8e7943063e8ad5b81bdf2ab2710cd14eb
Nerijus Arlauskas <nercury@gmail.com>
2015-02-21 01:17:21 +0200

    Simplify vbo binding.

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,9 +1,6 @@
 use gl;
 use gl::types::*;
 
-use std::rc::Rc;
-use context::buffer_target;
-
 pub struct Buffer {
     id: GLuint,
 }
@@ -27,17 +24,6 @@ impl Buffer {
     /// Returns true if contains correct buffer object.
     pub fn is_buffer(&self) -> bool {
         unsafe { gl::IsBuffer(self.id) == gl::TRUE }
-    }
-}
-
-pub trait BindableBuffer {
-    fn bind<'a, T>(&self, target: &'a mut T) -> &'a mut T where T : buffer_target::BindableBufferTarget;
-}
-
-impl BindableBuffer for Rc<Buffer> {
-    fn bind<'a, T>(&self, target: &'a mut T) -> &'a mut T where T : buffer_target::BindableBufferTarget {
-        target.bind(self);
-        target
     }
 }
 
--- src/context/buffer_target.rs
+++ src/context/buffer_target.rs
@@ -1,42 +1,18 @@
 use gl;
-use gl::types::*;
 
 use buffer::Buffer;
 use std::rc::Rc;
-use std::ops::Deref;
-
-pub trait BaseBufferTarget {
-    fn get_buffer(&self) -> Option<&Buffer>;
-}
-
-macro_rules! extend_base_buffer_target {
-    ( $Type:ty ) => {
-        impl BaseBufferTarget for $Type {
-            fn get_buffer(&self) -> Option<&Buffer> {
-                match (*self).buffer {
-                    Some(ref value) => Some(value),
-                    None => None,
-                }
-            }
-        }
-    };
-}
-
-pub trait BindableBufferTarget {
-    fn bind(&mut self, buffer: &Rc<Buffer>) -> Result<(), BindBufferError>;
-    fn unbind(&mut self);
-}
 
 macro_rules! impl_bindable_buffer_target {
     ( $Type:ty, $GLtarget:path ) => {
-        impl BindableBufferTarget for $Type {
-            fn bind(&mut self, buffer: &Rc<Buffer>) -> Result<(), BindBufferError> {
+        impl $Type {
+            pub fn bind(&mut self, buffer: &Rc<Buffer>) -> Result<&mut $Type, BindBufferError> {
                 self.buffer = Some(buffer.clone());
                 unsafe { gl::BindBuffer($GLtarget, buffer.get_id()) };
-                Ok(())
+                Ok(self)
             }
 
-            fn unbind(&mut self) {
+            pub fn unbind(&mut self) {
                 unsafe { gl::BindBuffer($GLtarget, 0) };
                 self.buffer = None;
             }
@@ -49,9 +25,6 @@ macro_rules! impl_bindable_buffer_target {
         }
     };
 }
-
-extend_base_buffer_target!(ArrayBufferTarget);
-extend_base_buffer_target!(ElementArrayBufferTarget);
 
 impl_bindable_buffer_target!(ArrayBufferTarget, gl::ARRAY_BUFFER);
 impl_bindable_buffer_target!(ElementArrayBufferTarget, gl::ELEMENT_ARRAY_BUFFER);
