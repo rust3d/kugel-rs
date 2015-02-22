a0d82dc98d36238ba2ac21bcce12bccf8e037702
Nerijus Arlauskas <nercury@gmail.com>
2015-02-22 22:39:29 +0200

    Direct state access for vao.

--- src/va.rs
+++ src/va.rs
@@ -14,6 +14,16 @@ impl VertexArray {
         self.id
     }
 
+    pub fn enable_attrib(&self, index: GLuint) {
+        debug!("[{}]: enable attrib, index = {}", self.id, index);
+        unsafe { gl::EnableVertexArrayAttrib(self.id, index) };
+    }
+
+    pub fn disable_attrib(&self, index: GLuint) {
+        debug!("[{}]: disable attrib, index = {}", self.id, index);
+        unsafe { gl::DisableVertexArrayAttrib(self.id, index) };
+    }
+
     /// Returns true if contains correct vertex array object and it
     /// was bound at least once.
     pub fn is_vertex_array(&self) -> bool {
